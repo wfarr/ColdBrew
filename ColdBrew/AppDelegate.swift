2e8dbdb3ab0007fee7993e6d516889cd32e1511a
Will Farrington <wcfarrington@gmail.com>
2016-04-30 12:48:00 -0400

    Initial commit

--- ColdBrew/AppDelegate.swift
+++ ColdBrew/AppDelegate.swift
@@ -11,17 +11,78 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
+    var caffeinateProcess: NSTask?
+    var disabled = true
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let icon = NSImage(named: "statusbar-icon")!
+        icon.template = true
+        
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        switchToDecaf()
     }
 
+    @IBAction func thirtyMinutesClicked(sender: NSMenuItem) {
+        caffeinateFor(30)
+    }
+    
+    @IBAction func oneHourClicked(sender: NSMenuItem) {
+        caffeinateFor(hours: 1)
+    }
 
+    @IBAction func threeHoursClicked(sender: NSMenuItem) {
+        caffeinateFor(hours: 3)
+    }
+    
+    @IBAction func oneDayClicked(sender: NSMenuItem) {
+        caffeinateFor(hours: 24)
+    }
+    
+    @IBAction func deactivateClicked(sender: NSMenuItem) {
+        switchToDecaf()
+        disabled = true
+    }
+
+    @IBAction func menuQuitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
+
+    func caffeinateFor(minutes: NSNumber = 0, hours: NSNumber = 0) {
+        // if we are re-caffeinating, make sure we kill the old caffeinate first
+        switchToDecaf()
+        
+        var duration = 0
+        
+        if (hours.integerValue > 0) {
+            duration += hours.integerValue * 60 * 60
+        }
+        
+        if (minutes.integerValue > 0) {
+            duration += minutes.integerValue * 60
+        }
+        
+        caffeinateProcess = NSTask()
+        caffeinateProcess!.launchPath = "/usr/bin/caffeinate"
+        caffeinateProcess!.arguments = ["-u", "-t", String(duration)]
+        
+        caffeinateProcess!.launch()
+        
+        disabled = false
+    }
+    
+    func switchToDecaf() {
+        if (caffeinateProcess?.running == true) {
+            caffeinateProcess?.terminate()
+            caffeinateProcess?.waitUntilExit()
+        }
+    }
 }
 
